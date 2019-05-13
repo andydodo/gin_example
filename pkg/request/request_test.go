1cbb384fd6c97c916144152e7c8f52b6e639007e
andydodo <andy415do@gmail.com>
2019-05-14 00:30:53 +0800

    feat(pkg): add max work nums for request and modify utils file name (#20)

--- pkg/request/request_test.go
+++ pkg/request/request_test.go
@@ -8,6 +8,7 @@ import (
 func TestReq_Request(t *testing.T) {
 	req := NewRequest()
 	req.Url = "https://www.itjuzi.com/api/index/banner"
+	req.MaxNums = 100
 	resp, err := req.Request()
 	fmt.Println(string(resp), err)
 }
